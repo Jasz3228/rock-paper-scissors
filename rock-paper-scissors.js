092796c63483894cf59894c21fd6f09cfa8a27c1
Jordan Stewart <{ID}+{username}@users.noreply.github.com>
2025-10-15 14:47:29 -0400

    Created function for random computer choice.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,3 +1,18 @@
 const log = console.log;
 
-log("Hello World");
+const randomNumber = Math.floor(Math.random() * 3);
+
+
+function getComputerChoice() {
+  if (randomNumber == 0) {
+    return "Rock";
+  } else if ( randomNumber == 1) {
+    return "Paper";
+  } else {
+    return "scissors";
+  }
+}
+
+
+
+log(getComputerChoice());

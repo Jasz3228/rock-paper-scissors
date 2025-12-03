8d4ace3ac279f4db9743b6c7b1b05084c55ac1e9
Jordan Stewart <149850673+Jasz3228@users.noreply.github.com>
2025-12-03 13:02:50 -0500

    Added Refrences and event listeners to the buttons refactored playRound function

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,57 +1,63 @@
+// function playGame() {
+//   let humanScore = 0;
+//   let computerScore = 0;
+
+
 
 function getComputerChoice() {
   const randomNumber = Math.floor(Math.random() * 3);
 
   if (randomNumber == 0) {
-    return "rock";
+    return "Rock";
   } else if ( randomNumber == 1) {
-    return "paper";
+    return "Paper";
   } else {
-    return "scissors";
+    return "Scissors";
   }
 }
 
-function getHumanChoice() {
-  let choice = prompt("Choose Rock, Paper, or Scissors")
-  return choice.toLowerCase(); 
-}
+  function playRound(humanChoice) {
+    const computerChoice = getComputerChoice();
 
-function playGame() {
-  let humanScore = 0;
-  let computerScore = 0;
-
-  function playRound(humanChoice, computerChoice) {
     if (humanChoice === computerChoice) {
       return "It's a tie!";
     } else if (
-      (humanChoice === "rock" && computerChoice == "scissors") ||
-      (humanChoice === "paper" && computerChoice === "rock") ||
-      (humanChoice === "scissors" && computerChoice === "paper")
+      (humanChoice === "Rock" && computerChoice == "Scissors") ||
+      (humanChoice === "Paper" && computerChoice === "Rock") ||
+      (humanChoice === "Scissors" && computerChoice === "Paper")
     ) {
-      humanScore++;
+      //humanScore++;
       return `You win! ${humanChoice} beats ${computerChoice}`;
     } else {
-      computerScore++;
+      //computerScore++;
       return `You lose! ${computerChoice} beats ${humanChoice}`;
     }
   }
-  for (let round = 1; round <= 5; round++) {
-  console.log(`\n--- Round ${round} ---`);
+  // for (let round = 1; round <= 5; round++) {
+  // console.log(`\n--- Round ${round} ---`);
   
-  const humanSelection = getHumanChoice();
-  const computerSelection = getComputerChoice();
+  // Getting references to the games buttons
 
-  const result = playRound(humanSelection, computerSelection);
-  console.log(result);
-  console.log(`Score: You ${humanScore} - Computer ${computerScore}`);
-  }
-}
+  const rockButton = document.getElementById("rock");
+  const paperButton = document.getElementById("paper");
+  const scissorsButton = document.getElementById("scissors");
 
+  // Adding event listeners for the buttons
 
-playGame();
+  rockButton.addEventListener("click", () => {
+    console.log(playRound("Rock"));
+  });
 
-//log(getHumanChoice());
-//log(getComputerChoice());
-//console.log(playRound(humanSelection, computerSelection));
+  paperButton.addEventListener("click", () => {
+    console.log(playRound("Paper"));
+  });
+
+  scissorsButton.addEventListener("click", () => {
+    console.log(playRound("Scissors"));
+  });
+     
+// console.log(`Score: You ${humanScore} - Computer ${computerScore}`);
+// }
+//}
 
 
